5032611a1df75d57d9f256b27a7e99615dbb0952
Ben <howdu@users.noreply.github.com>
2026-04-30 23:22:30 +0100

    feat: store form submissions

---
+++ packages/forms/src/Events/FormSubmitted.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Capell\Forms\Events;
+
+use Capell\Forms\Models\Form;
+use Capell\Forms\Models\Submission;
+use Illuminate\Foundation\Events\Dispatchable;
+
+class FormSubmitted
+{
+    use Dispatchable;
+
+    public function __construct(
+        public Form $form,
+        public Submission $submission,
+    ) {}
+}
