a598f5fc8ac3188c09e603f1f3f01eb643813dad
Ben <howdu@users.noreply.github.com>
2026-05-06 22:37:24 +0100

    fix: inherit content section type factory defaults

--- packages/content-sections/database/factories/ContentTypeFactory.php
+++ packages/content-sections/database/factories/ContentTypeFactory.php
@@ -6,23 +6,14 @@ namespace Capell\ContentSections\Database\Factories;
 
 use Capell\ContentSections\Enums\LayoutTypeEnum;
 use Capell\Core\Database\Factories\TypeFactory;
-use Illuminate\Support\Str;
 
 class ContentTypeFactory extends TypeFactory
 {
     public function definition(): array
     {
         return [
-            'name' => 'Section',
-            'key' => 'section-' . Str::random(8),
+            ...parent::definition(),
             'type' => LayoutTypeEnum::Section->value,
-            'default' => false,
-            'group' => null,
-            'admin' => [
-                'configurator' => 'Default',
-            ],
-            'created_at' => now()->subYear(),
-            'updated_at' => now()->subMonths(5),
         ];
     }
 }
