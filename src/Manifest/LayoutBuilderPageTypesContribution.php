55f47c5df185a795a4222e765b388594ed8a5fb3
Ben <howdu@users.noreply.github.com>
2026-06-15 12:16:01 +0100

    fix: expose layout builder runtime metadata

---
+++ src/Manifest/LayoutBuilderPageTypesContribution.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Capell\LayoutBuilder\Manifest;
+
+use Capell\Core\Contracts\Extensions\ExtensionContribution;
+use Capell\Core\Contracts\Extensions\RegistersExtensionPageType;
+
+final class LayoutBuilderPageTypesContribution implements ExtensionContribution, RegistersExtensionPageType
+{
+    public static function compatibleCapellApiVersion(): string
+    {
+        return '^4.0';
+    }
+}
